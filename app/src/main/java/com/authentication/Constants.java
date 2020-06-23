a170a28a691c52abbe3c32b74fa92edca956654c
Paul <paul.wuest>
2020-06-23 14:58:24 +0200

    changed base url to heroku server its online now ! :)

--- app/src/main/java/com/authentication/Constants.java
+++ app/src/main/java/com/authentication/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
      */
     public final static String EMAIL_REGEX = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
     public final static int PASSWORDLENGTH = 7;
-    public static final String BASE_URL = "http://10.0.2.2:8080/";
+    public static final String BASE_URL = "http://tickshare.herokuapp.com/";
     public final static String DATE_FORMAT = "MMM dd,yyy HH:mm";
     public final static String ACCOUNT_SUCCESS = "Your account has been succsessfully registerd";
     public final static String TRIP_SUCCESS = "Your trip has been succsessfully created ";
